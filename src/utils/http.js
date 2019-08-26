edd027de04b037108b29c550c24914e0eea71ecb
hongjin.wang <hongjin.wang@mobilemd.cn>
2019-08-26 14:50:19 +0800

    添加拦截器

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,6 +1,6 @@
 import http from 'axios';
 
-export default http.create({
+const $http = http.create({
     baseURL: '/api/site-web',
     timeout: 50000000,
     headers: {
@@ -9,3 +9,52 @@ export default http.create({
         'TM-Header-UserId': undefined
     }
 });
+
+$http.interceptors.response.use(
+    function(response) {
+        let data = response.data;
+
+        if (data) {
+            if (data.success) {
+                return data.data;
+            } else if (data.success === false) {
+                let code = data.errors[0].code;
+                let message = data.errors[0].message;
+
+                if (code === '405') {
+                    alert('无权限访问');
+                    location.href = '/login';
+                } else if (code === '406') {
+                    alert('帐号在异地登录，请重新登录');
+                    location.href = '/login';
+                } else {
+                    return Promise.reject(message);
+                }
+            }
+        } else {
+            return Promise.reject('服务器错误');
+        }
+    },
+    function(error) {
+        return Promise.reject(error);
+    }
+);
+
+export default $http;
+
+/*
+ *  后端返回格式模板示例：
+    {
+        "success": false,
+        "errors": [{
+            "internationalized": true,
+            "code": "405",
+            "message": "token is null or not exists"
+        }]
+    }
+
+    {
+        "success": true,
+        "data": [{a: 1}]
+    }
+*/
