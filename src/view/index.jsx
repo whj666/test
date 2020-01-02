3cc6a8b305a6cf6dbae148d76f7c97f060a94ee4
wang_hong_jin <1010349053@qq.com>
2020-01-02 13:15:57 +0800

    +1

--- src/view/index.jsx
+++ src/view/index.jsx
@@ -1,14 +1,8 @@
 import React from 'react';
-import RouteTest from './route';
 
 class App extends React.Component {
     render() {
-        return (
-            <React.Fragment>
-                hello world
-                <RouteTest />
-            </React.Fragment>
-        );
+        return <React.Fragment>hello world</React.Fragment>;
     }
 }
 
