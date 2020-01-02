a4f368d8abae6c22525e185abb86113ebf1dd197
wang_hong_jin <1010349053@qq.com>
2020-01-02 13:14:52 +0800

    去冗余

--- src/utils/action.js
+++ src/utils/action.js
@@ -15,39 +15,3 @@ export const queryMeetingList = params => {
         }
     });
 };
-
-// 示例
-// import React from 'react';
-// import { queryMeetingList } from 'utils/action.js';
-// import { LoadingHoc } from 'component/LoadingHoc';
-
-// @LoadingHoc
-// class App extends React.Component {
-//     async componentDidMount() {
-//         this.props.toggleLoading();
-//         await this.init();
-//         this.props.toggleLoading();
-//     }
-
-//     //请求
-//     init = async () => {
-//         try {
-//             const res = await queryMeetingList({
-//                 siteId: '027028eb7553472c9d94a650001af23a',
-//                 pageNum: 1,
-//                 pageSize: 20,
-//                 queryType: 'hand'
-//             });
-
-//             console.log(res);
-//         } catch (error) {
-//             console.log(error);
-//         }
-//     };
-
-//     render() {
-//         return <React.Fragment>hello world</React.Fragment>;
-//     }
-// }
-
-// export default App;
